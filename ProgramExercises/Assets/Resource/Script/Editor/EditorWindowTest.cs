3fb6b8b7fb14be2c7db11b72ade0e4b5be5dde33
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-09-14 04:14:34 +0900

    マテリアルのエディタ拡張
    特定のオブジェクト群のマテリアルをコピー元のオブジェクトのマテリアルに反映させる

---
+++ ProgramExercises/Assets/Resource/Script/Editor/EditorWindowTest.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+public class EditorWindowTest : EditorWindow
+{
+#if UNITY_EDITOR
+    [MenuItem("EditorWindowTest/オブジェクトをまとめてコピー")]
+    private static void ShowWindow()
+    {
+        var window = GetWindow<EditorWindowTest>("UIElements");
+        window.titleContent = new GUIContent("EditorWindowTest");
+        window.Show();
+    }
+
+    [SerializeField] private VisualTreeAsset _rootVisualTreeAsset;
+    [SerializeField] private StyleSheet _rootStyleSheet;
+
+    private void CreateGUI()
+    {
+        _rootVisualTreeAsset.CloneTree(rootVisualElement);
+        rootVisualElement.styleSheets.Add(_rootStyleSheet);
+
+        var copyButton = rootVisualElement.Q<Button>("copy_btn");
+        copyButton.clicked += () =>
+        {
+            var moto1 = (GameObject)rootVisualElement.Q<ObjectField>("moto1_obj").value;
+            var saki1 = (GameObject)rootVisualElement.Q<ObjectField>("saki1_obj").value;
+            var saki2 = (GameObject)rootVisualElement.Q<ObjectField>("saki2_obj").value;
+            var saki3 = (GameObject)rootVisualElement.Q<ObjectField>("saki3_obj").value;
+            saki1.GetComponent<Renderer>().sharedMaterial = moto1.GetComponent<Renderer>().sharedMaterial;
+            saki2.GetComponent<Renderer>().sharedMaterial = moto1.GetComponent<Renderer>().sharedMaterial;
+            saki3.GetComponent<Renderer>().sharedMaterial = moto1.GetComponent<Renderer>().sharedMaterial;
+        };
+    }
+#endif
+}
