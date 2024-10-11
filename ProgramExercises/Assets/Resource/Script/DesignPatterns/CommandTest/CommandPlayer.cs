d3d92ed36fbfa1347c8e187e3a1b922d4cac5344
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-10-11 23:07:48 +0900

    Commandパターンまで

---
+++ ProgramExercises/Assets/Resource/Script/DesignPatterns/CommandTest/CommandPlayer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class CommandPlayert : MonoBehaviour
+{
+    [SerializeField] CommandTest _commandTest;
+    private float moveInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ICommand moveLeft = new MoveCmd(_commandTest, -1f);
+            _commandTest.ExecuteCommand(moveLeft);
+        }
+
+        // Dキーで右に移動
+        if (Input.GetKey(KeyCode.D))
+        {
+            ICommand moveRight = new MoveCmd(_commandTest, 1f);
+            _commandTest.ExecuteCommand(moveRight);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ICommand jumpCommand = new JumpCmd(_commandTest);
+            _commandTest.ExecuteCommand(jumpCommand);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z)) _commandTest.UndoCommand();
+    }
+}
---
+++ ProgramExercises/Assets/Resource/Script/DesignPatterns/CommandTest/CommandTest.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICommand
+{
+    void Execute();
+    void Undo();
+}
+
+public class MoveCmd : ICommand
+{
+    private CommandTest _commandTest;
+    private float moveDistance;
+
+    public MoveCmd(CommandTest character, float moveDistance)
+    {
+        this._commandTest = character;
+        this.moveDistance = moveDistance;
+    }
+
+    //コマンドの実行
+    public void Execute()
+    {
+        _commandTest.MoveHorizontal(moveDistance);
+    }
+
+    //コマンドの取り消し(元の位置に戻す)
+    public void Undo()
+    {
+        _commandTest.MoveHorizontal(-moveDistance);
+    }
+}
+
+public class JumpCmd : ICommand
+{
+    private CommandTest _commandTest;
+    private Vector2 originalPosition;
+
+    public JumpCmd(CommandTest _commandTest)
+    {
+        this._commandTest = _commandTest;
+        originalPosition = _commandTest.transform.position;
+    }
+
+    public void Execute()
+    {
+        _commandTest.Jump();
+    }
+
+    public void Undo()
+    {
+        _commandTest.transform.position = originalPosition;
+    }
+}
+
+public class CommandTest : MonoBehaviour
+{
+    private Stack<ICommand> commandHistory = new();
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] Rigidbody2D playerRig2D;
+
+    public void ExecuteCommand(ICommand command)
+    {
+        command.Execute();
+        commandHistory.Push(command); // 実行したコマンドを履歴に保存
+    }
+
+    public void UndoCommand()
+    {
+        if (commandHistory.Count > 0)
+        {
+            ICommand lastCommand = commandHistory.Pop();
+            lastCommand.Undo();
+            Debug.LogWarning("コマンドをUndoします");
+        }
+    }
+
+    public void MoveHorizontal(float moveInput)
+    {
+        playerRig2D.velocity = new Vector2(moveInput * moveSpeed, playerRig2D.velocity.y);
+    }
+
+    public void Jump()
+    {
+        playerRig2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
